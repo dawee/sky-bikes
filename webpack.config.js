5960f7921088b7365e9f57643f6d27844ef596e1
David Corticchiato <david.corticchiato@gmail.com>
2018-03-08 23:22:25 +0100

    add watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'skybikes.js',
     path: path.resolve(__dirname, 'dist')
