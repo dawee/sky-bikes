2ff321f7621b389113e7d2c218441d2d1114abd0
David Corticchiato <david.corticchiato@gmail.com>
2018-03-08 23:28:23 +0100

    add webpack hotreload server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
   output: {
     filename: 'skybikes.js',
     path: path.resolve(__dirname, 'dist')
