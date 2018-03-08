4f1d1f7c81c45ca69b1020ea7a9c8ffa65b2d9a9
David Corticchiato <david.corticchiato@gmail.com>
2018-03-09 00:11:39 +0100

    feed db on request object

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://localhost/skybikes');
+
+app.use((req, res, next) => {
+  req.db = db;
+
+  next();
+});
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
 }));
 
-app.listen(3000, function () {
-  console.log('Skybikes is now running on port 3000');
-});
+app.listen(3000, () => console.log('Skybikes is now running on port 3000'));
