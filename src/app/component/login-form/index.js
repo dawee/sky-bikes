214228ee5b0e9863bb8a6e18140b91f182148334
David Corticchiato <david.corticchiato@gmail.com>
2018-03-13 11:44:21 +0100

    link from login to register

--- src/app/component/login-form/index.js
+++ src/app/component/login-form/index.js
@@ -9,11 +9,16 @@ const render = () => {
     const formNode = node.querySelector('form');
     const emailNode = node.querySelector('.email');
     const submitNode = node.querySelector('.submit');
+    const registerLinkNode = node.querySelector('.register-link');
 
     node.classList.add('login-form');
     emailNode.setAttribute('placeholder', email.placeholder);
     emailNode.setAttribute('value', email.value);
     submitNode.setAttribute('value', submit.title);
+    registerLinkNode.querySelector('.sentence').textContent =
+      'No account yet ?';
+    registerLinkNode.querySelector('a').textContent = 'Click here to register';
+    registerLinkNode.querySelector('a').setAttribute('href', '/register');
 
     updateHandler('formSubmit', formNode, 'submit', submit.handler);
     updateHandler('emailInput', emailNode, 'input', email.onInput);
